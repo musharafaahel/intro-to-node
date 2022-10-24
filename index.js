21963b7ae8cf45dfa82e626a80719b2796cf9922
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2022-10-24 11:32:38 +0530

    Initial learning of node.js and npm

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes =  require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+
+console.log("Hero Name : ",mySuperHeroName);
+console.log("Villan Name : ",mySuperVillainName)
